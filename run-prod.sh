0cd1440c843221bce68ba5be26a037e648b86027
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-18 07:48:17 -0300

    rodando em http para evitar compra de certificado https para funcionar o blazor com ws

--- run-prod.sh
+++ run-prod.sh
@@ -1,2 +1,4 @@
-dotnet publish -o Release
-nohup dotnet Release/blazor-desafio21dias-dotnet7.dll --urls http://localhost:5001 &
+# dotnet publish -o Release
+# nohup dotnet Release/blazor-desafio21dias-dotnet7.dll --urls http://localhost:5001 &
+
+nohup dotnet run --urls http://localhost:5001 &
