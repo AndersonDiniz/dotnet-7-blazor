b3037b0b6a4e20b53d6f47cb0041ab8aa8cfb6e4
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-12-18 07:41:24 -0300

    tentativa de rodar bazor em https

--- Program.cs
+++ Program.cs
@@ -3,11 +3,28 @@ using Microsoft.AspNetCore.Components.Web;
 using blazor_desafio21dias_dotnet7.Data;
 using blazor_desafio21dias_dotnet7.Data.Servicos;
 using blazor_desafio21dias_dotnet7.Ambientes;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+    options.ExcludedHosts.Add("example.com");
+    options.ExcludedHosts.Add("www.example.com");
+});
+
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = (int)HttpStatusCode.TemporaryRedirect;
+    options.HttpsPort = 5001;
+});
+
+
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ClienteServico>();
 builder.Services.AddSingleton<AdministradorServico>();
@@ -17,12 +34,12 @@ Configuracao.Host = builder.Configuration["Host"];
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
